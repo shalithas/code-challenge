9398a9151fbdffbb7f0be434794fd51da182d8cc
Shalitha Senarathne <shalitha89@gmail.com>
2022-03-13 23:54:01 +0800

    parse function logic completed

--- src/engine.js
+++ src/engine.js
@@ -2,7 +2,29 @@
 export const next = (world) => {
   return world;
 };
-// (pattern: string) => boolean[][]
+
+/**
+ * Convert string pattern into Array of Array
+ * 
+ * @param {string} pattern 
+ * @returns Array[]
+ */
 export const parse = (pattern) => {
-  return [];
+  let grid = [];
+  try {
+    const rows = pattern.split('\n');
+    grid = rows.map(row => {
+      const cols = [];
+      for (var i = 0; i < row.length; i++) {
+        const char = row.charAt(i);
+        cols.push(char === '.' ? 0 : 1);
+      }
+
+      return cols;
+    });
+  } catch (error) {
+    console.error(error);
+  }
+
+  return grid;
 };
